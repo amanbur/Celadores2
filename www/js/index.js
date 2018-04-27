029403b030c6c74550109cb2db0eba2581be0b1d
ADRIAN MANSO BURGOS <Q003874@red.nisa>
2018-04-27 18:17:26 +0200

    27/04/2018

--- www/js/index.js
+++ www/js/index.js
@@ -41,7 +41,7 @@ var app = {
         console.log('calling push init');
         var push = PushNotification.init({
             "android": {
-                "senderID": "XXXXXXXX"
+                "senderID": "1012612783499"
             },
             "browser": {},
             "ios": {
